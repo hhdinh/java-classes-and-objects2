7c4c7d75566313af5eead0d2770b90453fd31129
Hoa Dinh <hoadinh.rdms@gmail.com>
2021-02-16 01:52:00 -0600

    created Calculator class and added 5 methods

---
+++ javaClassesObjects2/src/javaClassesObjects2/Calculator.java
@@ -0,0 +1,24 @@
+package javaClassesObjects2;
+
+public class Calculator {
+
+	public int add(int num1, int num2) {
+		return num1 + num2;
+	}
+
+	public int subtract(int num1, int num2) {
+		return num1 - num2;
+	}
+
+	public int multiply(int num1, int num2) {
+		return num1 * num2;
+	}
+
+	public int divide(int num1, int num2) {
+		return num1 / num2;
+	}
+
+	public int square(int num1) {
+		return (int) Math.sqrt(num1);
+	}
+}
