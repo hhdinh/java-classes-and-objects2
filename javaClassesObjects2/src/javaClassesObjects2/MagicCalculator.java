3bdffe52f8a629b292a3cbc9754b3bf51088e189
Hoa Dinh <hoadinh.rdms@gmail.com>
2021-02-16 12:56:01 -0600

    added the scanner class and updated formatting

--- javaClassesObjects2/src/javaClassesObjects2/MagicCalculator.java
+++ javaClassesObjects2/src/javaClassesObjects2/MagicCalculator.java
@@ -1,11 +1,17 @@
 package javaClassesObjects2;
 
+import java.util.Scanner;
+
 public class MagicCalculator extends Calculator {
 	public static void main(String args[]) {
+		Calculator calculator = new Calculator();
+		Scanner scanner = new Scanner(System.in);
+
 		// finds the factorial
+		System.out.print("Enter a number: ");
+		int num = scanner.nextInt();
 		int i;
 		int fact = 1;
-		int num = 5; // number to use to calculate
 
 		for (i = 1; i <= num; i++) {
 			fact = fact * i;
@@ -13,16 +19,18 @@ public class MagicCalculator extends Calculator {
 
 		System.out.println("Factorial of " + num + " is " + fact);
 
-		Calculator calculator = new Calculator();
-
 		// calling the square method from the Calculator class
 		int sqNum = calculator.square(num);
 		int sqRt = (int) Math.sqrt(sqNum);
-		System.out.println("Square root of " + sqNum + " is " + sqRt);
+		System.out.println("Square root of " + sqNum + " is " + sqRt + "\n");
 
 		// finds the sine, cosine, and tangent
-		double degrees = 45.0;
+		System.out.print("Enter angle in degrees: ");
+
+		double degrees = scanner.nextInt();
 		double radians = Math.toRadians(degrees); // convert degrees to radians
+
+		scanner.close();
 
 		// prints the sine, cosine, and tangent values
 		System.out.println("sin(" + degrees + ") = " + Math.sin(radians));
